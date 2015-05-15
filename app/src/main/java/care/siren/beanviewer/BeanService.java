e791661d0178d0063b63bc344db1652bddfa7907
Simon Kamronn <simon@kamronn.dk>
2015-05-15 12:35:09 +0200

    First commit

---
+++ app/src/main/java/care/siren/beanviewer/BeanService.java
@@ -0,0 +1,126 @@
+package care.siren.beanviewer;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import nl.littlerobots.bean.Bean;
+import nl.littlerobots.bean.BeanDiscoveryListener;
+import nl.littlerobots.bean.BeanListener;
+import nl.littlerobots.bean.BeanManager;
+
+public class BeanService extends Service {
+    private static final String TAG = BeanService.class.getSimpleName();
+    private final IBinder binder = new LocalBinder();
+
+    public static final String SCRATCH_RECV = "Scratch received";
+
+    Bean mBean;
+    BeanListener beanListener;
+    BeanDiscoveryListener beanDiscoveryListener;
+    String serialMsg = "";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        beanDiscoveryListener = new BeanDiscoveryListener() {
+
+            @Override
+            public void onBeanDiscovered(Bean bean) {
+                Log.d(TAG, "Discovered: " + bean.getDevice().getName());
+                if (bean.getDevice().getName().equals("SIREN Bean")) {
+                    Log.i(TAG, "Connecting to Bean");
+                    mBean = bean;
+                    mBean.connect(getApplicationContext(), beanListener);
+                }
+            }
+
+            @Override
+            public void onDiscoveryComplete(){
+
+            }
+        };
+
+        beanListener = new BeanListener() {
+            @Override
+            public void onConnected() {
+                Log.i(TAG, "Bean connected");
+            }
+
+            @Override
+            public void onConnectionFailed() {
+                Log.i(TAG, "Bean connection failed");
+
+            }
+
+            @Override
+            public void onDisconnected() {
+                Log.i(TAG, "Bean disconnected");
+
+            }
+
+            @Override
+            public void onSerialMessageReceived(byte[] bytes) {
+                String output = new String(bytes, 0, bytes.length); // Add read buffer to new string
+                serialMsg += output;
+                if (serialMsg.endsWith("\n")){
+                    Log.i(TAG, "Serial message: " + serialMsg);
+                    serialMsg = "";
+                }
+            }
+
+            @Override
+            public void onScratchValueChanged(int i, byte[] bytes) {
+                int[] result = new int[5];
+                for (int n = 0; n < bytes.length/2; n++) {
+                     result[n] = (bytes[n*2] & 0xff) |
+                                ((bytes[n*2+1] & 0xff) << 8);
+
+                }
+
+                // Send data to activity
+                sendMessage(SCRATCH_RECV, result);
+            }
+        };
+        BeanManager.getInstance().startDiscovery(beanDiscoveryListener);
+    }
+
+    // Send an Intent with the data
+    private void sendMessage(String action, int[] data) {
+        Intent intent = new Intent(action);
+        intent.putExtra("Data", data);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i(TAG, "BeanService started");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mBean != null) {
+            if (mBean.isConnected())
+                mBean.disconnect();
+        }
+    }
+
+    public BeanService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    public class LocalBinder extends Binder {
+        BeanService getService(){
+            return BeanService.this;
+        }
+    }
+}
---
+++ app/src/main/java/care/siren/beanviewer/MainActivity.java
@@ -0,0 +1,118 @@
+package care.siren.beanviewer;
+
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+    private String TAG = "BeanViewer";
+    BeanService beanService;
+    boolean mBound = false;
+    TextView scratchText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        scratchText = (TextView) findViewById(R.id.receivedData);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Intent intent = new Intent(this, BeanService.class);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter(BeanService.SCRATCH_RECV);
+        LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(broadcastReceiver);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mBound){
+            unbindService(mConnection);
+            mBound = false;
+        }
+    }
+
+    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            switch (intent.getAction()){
+                case BeanService.SCRATCH_RECV:
+                    int[] result = intent.getIntArrayExtra("Data");
+
+                    StringBuilder builder = new StringBuilder();
+                    for (int i : result) {
+                        builder.append(i);
+                        builder.append(" ");
+                    }
+                    scratchText.setText(builder.toString());
+
+                    Log.d(TAG, "Scratch data received: " + builder.toString());
+                    break;
+            }
+        }
+    };
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            Log.v(TAG, "BeanService connected");
+            BeanService.LocalBinder binder = (BeanService.LocalBinder) service;
+            beanService = binder.getService();
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            Log.v(TAG, "BeanService disconnected");
+            mBound = false;
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
